fc144490224afb4ea3608356203c7fdb5c884ba6
Rochelle Palma <palmarochelleg@gmail.com>
2020-01-12 00:41:36 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const apiKey = 'pefW7QOeIChiMN5oYxctfDQ5xBjfqp5gCL9ilnZb'; 
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
+const selectedStates = [];
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${key}=${params[key]}`)
+  return queryItems.join('&');
+}
+  
+function displayResults(responseJson) {
+  console.log(responseJson);
+  $('.results-list').empty();
+  for(let element of responseJson.data){
+    $('.results-list').append(
+      `<li><h3>${element.fullName}</h3>
+      <p>${element.description}</p>
+      <p><a href ="${element.url}" target="_blank">More Information</a></p>
+      </li>
+      <p><a href="${element.directionsUrl}" target="_blank">Directions</a></p>`  
+    );
+  }
+    $('.results').removeClass('hidden');
+    $('.error').addClass('hidden');
+}
+  
+function getParks(state, maxResults) {
+  const params = {
+    stateCode: state,
+    limit: maxResults,
+    api_key: apiKey  
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+
+  console.log(url);
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('.error').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchPark = $('select option:selected').each(function(){selectedStates.push($(this).val());})
+    const maxResults = $('#max-results').val();
+    getParks(searchPark, maxResults);
+  });
+}
+
+$(watchForm);
